863184e01c2df0dd50bd3730b262b0147f9aafe0
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2026-01-29 08:29:05 +0400

    Update npm install command in prefetch.sh
    Replaced npm install command to use specific module.

--- prefetch.sh
+++ prefetch.sh
@@ -52,8 +52,8 @@ echo "Installing packages to warm cache..."
 echo "--------------------------------------"
 
 # THIS is the important line — pulls full dependency tree
-npm install "${INSTALL_PKGS[@]}" --legacy-peer-deps
-
+#npm install "${INSTALL_PKGS[@]}" --legacy-peer-deps
+npm install @module-federation/vite --legacy-peer-deps
 echo "--------------------------------------"
 echo "🧹 Cleaning temp project (keeping cache)"
 cd "$BASE_DIR"
